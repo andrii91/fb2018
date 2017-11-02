9ab5f97bd1ee2967a77f1ecb71eea88bc7223a1f
Andrii Litvin <al@gogmat.com>
2017-11-02 15:18:41 +0200

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,7 @@ gulp.task('js', function() {
 });
 gulp.task('js_v', function() {
     gulp.src([
-            'src/js/vendor/jquery.js',
+            // 'src/js/vendor/jquery.js',
             // 'src/js/vendor/bootstrap.min.js',
             // 'src/js/vendor/inputmask.js',
             // 'src/js/vendor/jquery.inputmask.js',
